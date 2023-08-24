400d63f20e70dddab689d13a7b78ad3dbb393a1c
Jenish <jenishdhanani47@gmail.com>
2023-08-24 10:11:28 +0530

    Fix image size issue

--- src/blog/validator.py
+++ src/blog/validator.py
@@ -2,3 +2,4 @@ def validate_width_and_height(cls, value, values, type):
     assert (
         value >= 32 and value <= 1024 and value % 8 == 0
     ), f"{type} must be between 32 and 1024 and divisible by 8"
+    return value
