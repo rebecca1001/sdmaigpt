4f8e2b14ae3af0767ab92660051a979c34415efb
hevron-maor <maor.hevron.upwork@gmail.com>
2023-11-03 11:43:48 -0700

    update blog generate

--- src/blog/blog_controller.py
+++ src/blog/blog_controller.py
@@ -18,7 +18,7 @@ openai.api_key = Config.OPENAI_API_KEY
 OUTLINE_GENERATE_PROMPT = """
 Generate blog post outline with section and subsection titles.
 Make sure to only output outline and nothing else. 
-Generate BLOG_SUBSECTION_COUNT subsections in total.
+Generate BLOG_SECTION_COUNT sections in total.
 """
 
 SYSTEM_PROMPT = """
@@ -225,7 +225,7 @@ Language: {self.language}
 Spellings Format: {self.spellings_format}
         """
 
-        outline_prompt = OUTLINE_GENERATE_PROMPT.replace('BLOG_SUBSECTION_COUNT', str(int(self.length / 70)))
+        outline_prompt = OUTLINE_GENERATE_PROMPT.replace('BLOG_SECTION_COUNT', str(int(self.length / 150)))
         outline_prompt = outline_prompt + prompt
 
         print ('_' * 100)
